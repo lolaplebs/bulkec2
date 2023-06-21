85aa38ce2f8cb99927186ee38755e0d4e163576a
lolaplebs <77144649+lolaplebs@users.noreply.github.com>
2023-06-21 12:51:52 +0530

    adding ami

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-026ebd4cfe2c043b2"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
   tags = {
